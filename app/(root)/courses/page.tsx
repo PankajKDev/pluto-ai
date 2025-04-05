1a31dbc53c601b52eda50a80bd719e20bea83ff6
PankajKDev <pankajk.dev0000@gmail.com>
2025-04-05 23:39:48 +0530

    [Frontend]:Fixed Course Page Layout

--- app/(root)/courses/page.tsx
+++ app/(root)/courses/page.tsx
@@ -1,11 +1,18 @@
 import MyCourses from "@/components/shared/MyCourses";
+import PageHeading from "@/components/ui/PageHeading";
 
-function page() {
+const page = () => {
   return (
-    <div>
-      <MyCourses />
-    </div>
+    <>
+      <div className="px-4 py-10 md:py-20">
+        <PageHeading
+          headingText="Explore Our Courses"
+          subHeadingText="Unlock skills for every goal and passion."
+        />
+        <MyCourses />
+      </div>
+    </>
   );
-}
+};
 
 export default page;
