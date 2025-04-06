ed576b1fa71208a7b411c20aaea093a28ecbb0b5
PankajKDev <pankajk.dev0000@gmail.com>
2025-04-06 21:39:56 +0530

    [Backend]:returned plain courses instead of complex mongodb objects

--- lib/actions/fetch.action.ts
+++ lib/actions/fetch.action.ts
@@ -5,7 +5,8 @@ import connectDB from "../connectDB";
 export async function fetchInterviews(userId: string) {
   await connectDB();
   try {
-    const courses = await Course.find({ userid: userId });
+    const fetchedCourses = await Course.find({ userid: userId });
+    const courses = fetchedCourses.map((doc) => doc.toObject());
     return courses;
   } catch (error) {
     console.log("Error :", error);
